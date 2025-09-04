4a5d8bbe3c08652a7050ac31f3620ddc3c5cb0fb
Arunabha-NOTE <arunabham2017@gmail.com>
2025-09-05 02:19:24 +0530

    codebase setup

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,24 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Material from '@primeuix/themes/material';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
-  ]
+    providers: [
+        provideRouter(routes),
+        provideAnimationsAsync(),
+        providePrimeNG({
+            theme: {
+                preset: Material,
+                options: {
+                    primary: 'sky',
+                    surface: 'soho'
+                }
+            },
+            ripple: true
+        })
+    ]
 };
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import Lenis from 'lenis';
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
+
+const lenis = new Lenis();
+
+function raf(time: number) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
